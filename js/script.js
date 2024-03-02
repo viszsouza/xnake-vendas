34759cc77d365fe7b7003af1da21d0d2ea2195af
vizssouza <vini.nelso@gmail.com>
2024-03-02 00:06:12 -0300

    Atualizando CSS
    Ajustando Navbar, Titulo da coleção e Itens.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+  idx ++;
+
+  if(idx > img.length - 1){
+    idx = 0;
+  }
+
+  imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+setInterval(carrossel, 1800);
+
