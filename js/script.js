b80bbf35755aabafd781f83cc90b33069aa60e33
iadog <iago.exe@hotmail.com>
2024-06-01 18:41:57 -0300

    Adicionado o carrossel

--- js/script.js
+++ js/script.js
@@ -1,27 +1,7 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-
-const imgs = document.getElementById("img");
-const img = document.querySelectorAll("#img img");
-
-let idx = 0;
-
-function carrossel(){
-  idx ++;
-
-  if(idx > img.length - 1){
-    idx = 0;
-  }
-
-  imgs.style.transform = `translateX(${-idx * 500}px)`;
-}
-
-setInterval(carrossel, 1800);
-
+$(document).ready(function() {
+  $('.carousel').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+  });
+});
